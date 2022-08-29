4baef3ed0c8c595e695cfb706dbf670e376e9579
62858 <ugmlr2896@gmail.com>
2022-08-29 10:42:31 +0700

    Penjelasan variable dan type data beserta contoh!

---
+++ Variable&DataType.py
@@ -0,0 +1,70 @@
+
+# Variable
+
+# Variabel      = tempat untuk penyimpanan keterangan informasi
+# Assignment    = operator untuk memasukkan suatu nilai ke dalam variabel, terdapat 11 operator assignment
+# literal       = notasi untuk mewakili nilai tetap dalam variabel atau sebagai nilai mentah yang diberikan dalam variabel.
+
+
+Nama = "Fuchia" #Variabel(Nama), Assignment(=), Literal(Fuchia)
+print(Nama) #print digunakan untuk memanggil variabel yang akan dicetak.
+
+# Data Type
+
+# String (str)       = Data dikatakan string ketika diapit oleh tanda kutip
+# Integer (int)      = Data dikatakan integer ketika data(literal) bernilai bilangan bulat dan tanpa tanda kurung
+# Float (float)      = Data dikatakan float ketika data (literal) bernilai bilangan desimal dan tanpa tanda kurung
+# Boolean (bool)     = Tipe data ini hanya bernilai TRUE atau FALSE.
+                       # Ada 2 aturan dalam penggunaan Boolean.
+                       # Pertama, dalam penulisan harus menggunakan kapital pada awal kata (True) (False),
+                       # Kedua, data boolean tidak perlu kutip.
+# List [1,2,3]       = Sebuah kumpulan data/daftar yang mutable(Dapat diubah), terlampir dalam [ ] (kurung siku) dan dipisahkan oleh koma.
+# Sets {1,2,3}       = kumpulan tipe data yang tidak berurutan yang dapat diulang, dapat diubah, dan tidak memiliki elemen duplikat.
+                       # terlampir dalam { } (kurung kurawa) dan dipisahkan oleh koma.
+# Tuple (1,2,3)      = Kumpulan data yang tidak dapat diubah, dan memungkinkan nilai duplikat, terlampir dalam ( ) (kurung bulat) dan dipisahkan oleh koma.
+# Dictionary {a:1}   = untuk menyimpan nilai data dalam pasangan key:value dan dapat diubah. Tidak ada anggota duplikat.
+                       # terlampir dalam { Key : Value } dan dipisahkan oleh koma.
+
+# Sring
+
+Tipe_Sring = "Str"
+print(Tipe_Sring)
+
+# Integer
+
+Tipe_Integer = 1, 10, 100, 1000
+print(Tipe_Integer)
+
+# Float
+
+Tipe_Float = 2.86
+print(Tipe_Float)
+
+# Boolean
+
+Tipe_Boolean = True
+print(Tipe_Boolean)
+
+# List
+
+Tipe_List = [1, "a", 3, 4.1, True]
+print(Tipe_List)
+
+# Set
+
+Tipe_Set = {1, "a", 3, 4.5, 2, 4.2, True}
+print(Tipe_Set)
+
+# Tuple
+
+Tipe_Tuple = (1, 2, 3, 1, 12, 2, 31, "a", 3, 4.5, 2, 4.2, True)
+print(Tipe_Tuple)
+
+# Dictionary
+
+Tipe_Dict = {
+            "a" : 1,
+            "b" : 2,
+            "c" : 3,
+             }
+print(Tipe_Dict)
