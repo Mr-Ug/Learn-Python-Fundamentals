33a251b8afbe12c980e92218b52558e8c19e9735
Mr-Ug <112061079+Mr-Ug@users.noreply.github.com>
2022-09-06 11:00:55 +0700

    Karakteristik List Python!

---
+++ PythonList.py
@@ -0,0 +1,42 @@
+
+# PYTHON LIST
+
+# Adalah salah satu dari 4 tipe data bawaan di Python untuk menyimpan beberapa item dalam satu variabel
+
+#======================================================================================================================#
+
+# LIST
+# Karakteristik List :
+#     1. Data berurut
+#     2. Elemen list dapat diakses dengan indeks
+#     3. List bersifat mutable atai tipe data yang dapat diubah
+#     4. Dapat menyimpan jumlah berbagai elemen
+#     5. Diapit oleh kurung siku "[]"
+
+#======================================================================================================================#
+
+# TUPLE
+# Karakteristik Tuple :
+#     1. Tuple bersifat immutable atau tidak dapat diubah
+#     2. Diapit oleh kurung bulat "()"
+#     3. Dapat menyimpan jumlah berbagai elemen
+#     4. Elemen Tuple dapat diakses dengan indeks
+
+#======================================================================================================================#
+
+# SET
+# Karakteristik Set :
+#     1. Set bersifat unordered atau tidak bisa diakses dengan indeks.
+#     2. Set bersifat unchangable atau tidak bisa diubah lagi
+#     3. Tidak bisa menerima nilai duplikat
+#     4. diapit oleh kurung kurawa "{}"
+
+#======================================================================================================================#
+
+# DICTIONARY
+# Karakteristik Dict :
+#     1. memiliki Key & Value
+#     2. Data berurut
+#     3. bersifat immutable atau tidak dapat diubah
+#     4. Tidak bisa menerima nilai duplikat
+#     5. Diapit oleh kurung kurawa "{Key : Value}"
